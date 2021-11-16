23a9603bdc98d2e946aab5067682c6ca09bd6443
Mateus Capitanio <mateusjmj92@gmail.com>
2021-11-16 15:25:23 -0300

    exercicios de aprendizado

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,11 @@
+
+const paragraph = document.querySelector("#paragraph");
+paragraph.style.color = "green";
+
+const title = document.querySelector('#page-title');
+title.innerHTML = 'As branquelas'
+
+const paragraph2 = document.querySelector('#second-paragraph');
+paragraph2.style.color = 'blue'
+
+document.getElementsByName('subtitulo')[0].innerHTML = 'Título segundo';
